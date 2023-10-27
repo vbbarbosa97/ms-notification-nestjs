8cc18ae3b282529e5ab78a1411d52057c0d0174b
Vinicius Batista Barbosa <vinicius.batista.barbosa34@gmail.com.br>
2023-10-27 11:31:29 -0300

    teste notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('task_notification')
+  public taskNotification(data: any) {
+    console.log(data);
   }
 }
