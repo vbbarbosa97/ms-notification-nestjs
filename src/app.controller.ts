d308786f5ff3db5fbf92a19c80cba3997982e30e
Vinicius Batista Barbosa <vinicius.batista.barbosa34@gmail.com.br>
2023-10-30 15:34:46 -0300

    adiciona lib de envio de email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,27 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 
+export type NotificationDTO = {
+  email: string;
+  startAt: Date;
+  endAt: Date;
+  name: string;
+  title: string;
+  description: string;
+};
+
 @Controller()
 export class AppController {
+  constructor(private mailerService: MailerService) {}
+
   @EventPattern('task_notification')
-  public taskNotification(data: any) {
-    console.log(data);
+  public async taskNotification(data: NotificationDTO) {
+    await this.mailerService.sendMail({
+      to: data.email,
+      subject: 'Notificação da tarefa',
+      from: 'taskmanager@nestjscurso.com.br',
+      text: `Olá ${data.name}! Você tem a atividade ${data.title} que se inicia em ${data.startAt} e finaliza ${data.endAt}`,
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'concepcion.oberbrunner@ethereal.email',
+          pass: '99A1Qf9bZrYhAYcpeP',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
